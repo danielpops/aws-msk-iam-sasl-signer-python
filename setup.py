d64c12ae47c36b76e04c07d2e256a1e269326663
dpopes <danielpops@gmail.com>
2023-12-05 14:54:50 -0800

    Support python3.7
    The travis / github actions supports python3.7, and AFAICT the tests all pass with python3.7. For my use case I need a version of this package that I can install to a python3.7 environment.

--- setup.py
+++ setup.py
@@ -26,12 +26,13 @@ test_requirements = [
 
 setup(
     author="Amazon Managed Streaming for Apache Kafka",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
